3eab0ee1310948b95ba8739788320c074e65faf0
virchau13 <virchau13@hexular.net>
2021-10-31 01:34:35 +0800

    refactor: change from saving to /etc/nixos to saving into /etc/henix/{hash}

--- src/deploy.rs
+++ src/deploy.rs
@@ -1,68 +1,23 @@
 /// Does the actual deployment.
-use crate::{ssh, DeployOpts, NodeCfg};
+use crate::{DeployOpts, NodeCfg, nix, ssh};
 use anyhow::{anyhow, Context, Result};
 use std::{path::Path, process::Stdio};
 use tokio::process;
 use tracing::{error, info};
 
-async fn rollback(remote: &mut openssh::Session) -> Result<()> {
-    info!("Rolling back");
-    let rm = remote
-        .command("rm")
-        .arg("-r")
-        .arg("/etc/nixos")
-        .status()
-        .await
-        .context("Could not invoke rm to remove new /etc/nixos")?;
-    if !rm.success() {
-        return Err(anyhow!(format!(
-            "Could not remove new /etc/nixos (rm exited with {}), you will have to do it manually",
-            rm.code()
-                .map(|x| i32::to_string(&x))
-                .unwrap_or("<unknown>".to_owned())
-        )));
-    }
-    let mv = remote
-        .command("mv")
-        .arg("/etc/nixos.old")
-        .arg("/etc/nixos")
-        .output()
-        .await
-        .context("Could not invoke mv to restore /etc/nixos.old to /etc/nixos")?;
-    if !mv.status.success() {
-        return Err(anyhow!(format!(
-                    "Could not move /etc/nixos.old back to /etc/nixos (mv stderr: {}), you will have to do it manually",
-                    String::from_utf8_lossy(&mv.stderr)
-                )));
-    }
-    info!("Rollback complete");
+// Since we just save the config to `/etc/henix/{hash}`, it's not necessary to rollback anything
+// on build failure.
+async fn rollback(_remote: &mut openssh::Session) -> Result<()> {
+    info!("No special rollback necessary");
     Ok(())
 }
 
 async fn copy_config(
-    remote: &mut openssh::Session,
     node_location: &str,
     cfg_dir: &Path,
+    cfg_hash: &str,
 ) -> Result<()> {
     info!("Copying files");
-    info!("Moving the remote's /etc/nixos to /etc/nixos.old");
-    // Move the old `/etc/nixos` out of the way first.
-    let mv = remote
-        .command("mv")
-        .arg("/etc/nixos")
-        .arg("/etc/nixos.old")
-        .stderr(Stdio::inherit())
-        .status()
-        .await
-        .context("Could not invoke mv to move old /etc/nixos out of the way")?;
-    if !mv.success() {
-        return Err(anyhow!(format!(
-            "Could not move old /etc/nixos out of the way (mv exited with {}), aborting",
-            mv.code()
-                .map(|x| i32::to_string(&x))
-                .unwrap_or("<unknown>".to_owned())
-        )));
-    }
     info!("Using rsync to copy config");
     // We need to add a slash after `cfg_dir`,
     // so that rsync copies the *contents* of the directory,
@@ -74,10 +29,10 @@ async fn copy_config(
         .arg("-a") // Archive mode, preserve symlinks, permissions, devices, etc.
         .arg("-F") // Allow `.rsync-filter` files to be used
         .arg("--delete") // Remove files on the remote not present locally
-        .arg("-e")
-        .arg("ssh") // Use ssh (rsync might have been configured differently)
+        .arg("--mkpath") // Equivalent of `mkdir -p` on the remote path
+        .arg("-e").arg("ssh") // Use ssh (rsync might have been configured differently)
         .arg(cfg_dir_with_slash) // Copy the contents of the current directory...
-        .arg(format!("root@{}:/etc/nixos", node_location)) // to `/etc/nixos` on the remote
+        .arg(format!("root@{}:/etc/henix/{}", node_location, cfg_hash)) // to `/etc/henix/{hash}` on the remote
         .output()
         .await
         .context("Could not execute rsync to copy files")?;
@@ -100,16 +55,15 @@ async fn build_config(
     dep_opts: &DeployOpts,
     remote: &mut openssh::Session,
     node_name: &str,
+    cfg_hash: &str,
 ) -> Result<()> {
-    
     info!("Building config on remote");
     let mut rebuild = remote
         .command("nixos-rebuild");
     rebuild
         .arg(if dep_opts.boot { "boot" } else { "switch" })
         .arg("--flake")
-        .arg(format!("/etc/nixos#{}", node_name)); // FIXME this doesn't escape quotes in the name.
-
+        .arg(format!("/etc/henix/{}#{}", cfg_hash, node_name)); // FIXME this doesn't escape quotes in the name.
     let rebuild = ssh::proxy_output_to_logging("nixos-rebuild", rebuild).await.context("Rebuild execution failed")?;
     if !rebuild.success() {
         return Err(anyhow!("Rebuild failed"));
@@ -119,6 +73,7 @@ async fn build_config(
     Ok(())
 }
 
+/// Does the actual deployment, doesn't rollback on failure.
 async fn process_node_raw(
     dep_opts: &DeployOpts,
     remote: &mut openssh::Session,
@@ -126,28 +81,17 @@ async fn process_node_raw(
     node_cfg: &NodeCfg,
     cfg_dir: &Path,
 ) -> Result<()> {
-    copy_config(remote, &node_cfg.location, cfg_dir)
+    let cfg_hash = nix::hash(cfg_dir).await.context("Could not get hash")?;
+    copy_config(&node_cfg.location, cfg_dir, &cfg_hash)
         .await
         .context("Could not copy config")?;
-    build_config(dep_opts, remote, &name)
+    build_config(dep_opts, remote, &name, &cfg_hash)
         .await
         .context("Could not build config")?;
-    // if let Err(e) = build_res {
-    //     // If the forced rollback option is set, use it,
-    //     // else use the rollback_on_failure configuration.
-    //     if dep_opts.force_rollback_on_failure.unwrap_or(node_cfg.rollback_on_failure) {
-    //         rollback(remote).await.context("Could not rollback")?;
-    //     }
-    //     return Err(e);
-    // }
-    // if node_cfg.rollback_on_unreachable {
-    //     // dead man's switch
-    //     todo!()
-    // }
     Ok(())
 }
 
-// This handles the errors and the logging, `process_node_raw` does the actual work.
+/// Handles the errors, logging, and rollback; `process_node_raw` does the actual deployment.
 #[tracing::instrument(skip(dep_opts, node_cfg, cfg_dir))]
 pub async fn process_node(dep_opts: &DeployOpts, name: &str, node_cfg: NodeCfg, cfg_dir: &Path) {
     let mut remote;
--- src/nix.rs
+++ src/nix.rs
@@ -18,10 +18,28 @@ pub async fn eval<Schema: DeserializeOwned>(cfg_dir: &Path, arg: &str) -> anyhow
         .context("Could not execute nix eval command")?;
     if !out.status.success() {
         return Err(anyhow!(format!(
-            "Could not execute `nix eval {}` command:\n{}",
+            "Could not execute `nix eval {}` command, with stderr:\n{}",
             arg,
             &String::from_utf8_lossy(&out.stderr)
         )));
     }
     serde_json::from_slice(&out.stdout).context(format!("`{}` does not match JSON schema", arg))
+}
+
+/// Equivalent to `nix-hash "$dir"`.
+pub async fn hash(dir: &Path) -> anyhow::Result<String> {
+    let out = process::Command::new("nix-hash")
+        .arg(dir)
+        .output()
+        .await
+        .context("Could not execute nix-hash command")?;
+    if !out.status.success() {
+        return Err(anyhow!(format!(
+            "Could not execute `nix-hash {}` command, with stderr:\n{}",
+            dir.display(),
+            &String::from_utf8_lossy(&out.stderr)
+        )));
+    }
+    let hash = String::from_utf8(out.stdout).context("Could not decode nix-hash's output as UTF-8")?;
+    Ok(hash.trim().to_string())
 }
